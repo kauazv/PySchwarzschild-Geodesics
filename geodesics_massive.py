ccfd7ddc38bd3950bbb63ae6a24a7c818f475d0f
kauazv <kaua.policarpo04@gmail.com>
2026-01-28 01:12:36 -0300

    feat: integração númerica com evento de horizonte

--- geodesics_massive.py
+++ geodesics_massive.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 from schwarzschild import schwarzschild_factor
 
 def geodesic_equations(tau, y, M, E, L):
@@ -18,3 +19,32 @@ def geodesic_equations(tau, y, M, E, L):
         dphi_dtau,
         r_ddot
     ])
+
+def integrate_geodesic(
+        tau_span,
+        y0,
+        M,
+        E,
+        L,
+        r_stop=2.05,
+        max_step=0.1):
+    
+    def horizon_event(tau,y):
+        r = y [1]
+        return r - r_stop
+
+    horizon_event.terminal = True
+    horizon_event.direction = -1
+
+    solution = solve_ivp(
+        fun=lambda tau, y: geodesic_equations(tau, y, M, E, L),
+        t_span=tau_span,
+        y0=y0,
+        method="RK45",
+        max_step=max_step,
+        events=horizon_event,
+        rtol=1e-9,
+        atol=1e-9
+    )
+
+    return solution
