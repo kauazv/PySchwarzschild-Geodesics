0ef37243d17915879f316bc721948b7bfb3c40f5
kauazv <kaua.policarpo04@gmail.com>
2026-02-01 04:55:23 -0300

    feat: visualização de órbitas relastivísticas maassivas 

--- plots/plot_orbit_massive.py
+++ plots/plot_orbit_massive.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent[1]))
+sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -28,3 +28,16 @@ solution = integrate_geodesic(
 
 r = solution.y[1]
 phi = solution.y[2]
+
+x = r * np.cos(phi)
+y = r * np.sin(phi)
+
+plt.figure()
+plt.plot(x, y)
+plt.scatter([0], [0])
+plt.gca().set_aspect("equal", adjustable="box")
+plt.title("Órbita relativística em Schwarzschild")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.grid(True)
+plt.show()
